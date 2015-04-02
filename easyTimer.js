749321a56bc5754187b7798d1c392b1fce4e26d2
kukko <nyariag@gmail.com>
2015-04-02 20:27:35 +0200

    Guaranteeing jQuery
    The script check how jQuery is loaded or not. If not, it's load jQuery
    v2.1.3 from Google ajax library. If jQuery is already loadad, it's do
    nothing with that.

---
+++ easyTimer.js
@@ -0,0 +1,25 @@
+﻿function loadJQ(src, callback){
+	if (!window.jQuery){
+		var jQ=document.createElement("script");
+		jQ.src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js";
+		jQ.async=true;
+		jQ.onreadystatechange=jQ.onload=function(){
+			var state=jQ.readyState;
+			if (!callback.done && (!state || /loaded|complete/.test(state))){
+				callback.done=true;
+				callback();
+			}
+		}
+		document.getElementsByTagName("head")[0].appendChild(jQ);
+	}
+	else{
+		callback.done=true;
+		callback();
+	}
+}
+function easyTimer(){
+	return $.fn.jquery;
+}
+loadJQ("ASD", function(){
+	console.log(easyTimer());
+});
